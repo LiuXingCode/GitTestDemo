889ddc80e97762767abc1345adc6a1e0b432f585
liuxing <liuxinghenau@163.com>
2018-03-29 20:22:35 +0800

    代码初始化

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by 刘行 on 2018/3/29.
+//  Copyright © 2018年 刘行. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
